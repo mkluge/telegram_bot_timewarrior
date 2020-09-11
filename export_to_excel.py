8264520e107d6dc108e0f033bdcb11906ac85009
Michael Kluge <vollseil@gmail.com>
2020-09-11 19:42:15 +0200

    convert json export to xlsx file

---
+++ export_to_excel.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+from datetime import date, timedelta
+
+tage = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+
+df = pd.read_json('timew_export.json')
+
+df['start'] = pd.to_datetime(df['start'], format='%Y%m%dT%H%M%SZ').dt.tz_localize(
+    'utc').dt.tz_convert('Europe/Berlin')
+df['end'] = pd.to_datetime(df['end'], format='%Y%m%dT%H%M%SZ').dt.tz_localize(
+    'utc').dt.tz_convert('Europe/Berlin')
+df['workmin'] = (df.end-df.start).astype('timedelta64[m]')
+df['tag'] = df.start.dt.date
+df['wd'] = df.start.dt.weekday  # Mo=1,So=7
+
+sdate = df.iloc[0].tag
+edate = df.iloc[-1].tag
+days = []
+delta = edate - sdate  # as timedelta
+for i in range(delta.days + 1):
+    day = sdate + timedelta(days=i)
+    days.append(day)
+
+df_target = pd.DataFrame(columns=["Datum", "Wochentag", "Start",
+                                  "Ende", "ArbeitszeitSoll", "ArbeitszeitIst", "Pausen", "Differenz"])
+
+for day in days:
+    newline = {}
+    newline["Datum"] = day.strftime("%d.%m.%Y")
+    newline["Wochentag"] = tage[day.weekday()]
+    if day.weekday() in [5, 6]:
+        newline["ArbeitszeitSoll"] = 0
+    else:
+        newline["ArbeitszeitSoll"] = 480
+    if not day in df.tag.values:
+        newline["Start"] = ""
+        newline["Ende"] = ""
+        newline["ArbeitszeitIst"] = 0
+        newline["Pausen"] = 0
+    else:
+        today = df[df.tag == day]
+        start = today.iloc[0].start
+        end = today.iloc[-1].end
+        total_minutes = np.ceil((end-start).total_seconds()/60)
+        newline["Start"] = start.strftime("%H:%M")
+        newline["Ende"] = end.strftime("%H:%M")
+        newline["ArbeitszeitIst"] = int(today["workmin"].sum())
+
+        newline["Pausen"] = total_minutes-newline["ArbeitszeitIst"]
+    newline["Differenz"] = newline["ArbeitszeitIst"]-newline["ArbeitszeitSoll"]
+    df_target = df_target.append(newline, ignore_index=True)
+
+df_target.to_excel("arbeitszeit.xlsx")
